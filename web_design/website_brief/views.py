9550a902ad6da464d4582bc631ae1e62496908b4
Dmitry Zhdanov <dzhdanov@scnsoft.com>
2024-06-28 15:55:06 +0300

    brief v0

--- web_design/website_brief/views.py
+++ web_design/website_brief/views.py
@@ -1,3 +1,30 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.core.mail import send_mail
+from .forms import WebsiteBriefForm
 
-# Create your views here.
+
+def website_brief(request):
+    if request.method == 'POST':
+        form = WebsiteBriefForm(request.POST)
+        if form.is_valid():
+            # Обработка данных формы
+            # Например, отправка email или сохранение в базу данных
+            send_email_with_form_data(form.cleaned_data)
+            return redirect('thank_you')  # Перенаправление на страницу благодарности
+    else:
+        form = WebsiteBriefForm()
+
+    return render(request, 'website_brief.html', {'form': form})
+
+
+def send_email_with_form_data(data):
+    subject = 'Новый бриф на разработку сайта'
+    message = '\n'.join([f'{key}: {value}' for key, value in data.items()])
+    from_email = 'your_email@example.com'
+    recipient_list = ['recipient@example.com']
+
+    send_mail(subject, message, from_email, recipient_list)
+
+
+def thank_you(request):
+    return render(request, 'thank_you.html')
