ec5ec2b794e9fa0bf0438d83543b8dbdc0e51db8
Dmitry Zhdanov <dzhdanov@scnsoft.com>
2024-07-02 12:52:54 +0300

    update thank_you url

--- web_design/web_design/urls.py
+++ web_design/web_design/urls.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
 from django.urls import path, include
-from .views import health_check
+from .views import health_check, thank_you
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('brief/', include('website_brief.urls')),
     path('health/', health_check, name='health_check'),
+    path('thank_you/', thank_you, name='thank_you'),
 ]
