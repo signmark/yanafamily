78e48674c91d68237f7d620b84a0d2e17178304f
Dmitry Zhdanov <signmark@gmail.com>
2024-12-02 11:40:10 +0300

    Removed old web_design and added yanafamily structure

--- web_design/renew-cert.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-docker run -it --rm --name certbot \
-  -v "$(pwd)/certbot/conf:/etc/letsencrypt" \
-  -v "$(pwd)/certbot/www:/var/www/certbot" \
-  certbot/certbot renew
-docker-compose restart nginx
--- web_design/website_brief/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.website_brief, name='index'),
-]
